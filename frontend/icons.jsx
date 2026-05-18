73b54745a34f52c7d3d30bbfd0ac5216c910f28b
Rustamaliev Mirshoir <96902648+Mirshoir@users.noreply.github.com>
2026-05-18 10:01:41 +0500

    Update icons.jsx

--- frontend/icons.jsx
+++ frontend/icons.jsx
@@ -5,6 +5,7 @@ const ICON_PROPS = { viewBox: '0 0 24 24', fill: 'none', stroke: 'currentColor',
 
 const I = {
   Inbox: (p) => (<svg width="18" height="18" {...ICON_PROPS} {...p}><path d="M22 12h-6l-2 3h-4l-2-3H2"/><path d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11Z"/></svg>),
+  Comment:(p) => (<svg width="18" height="18" {...ICON_PROPS} {...p}><path d="M21 15a2 2 0 0 1-2 2H8l-5 5V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/></svg>),
   Chart: (p) => (<svg width="18" height="18" {...ICON_PROPS} {...p}><path d="M3 3v18h18"/><path d="M7 14l4-4 4 4 5-7"/></svg>),
   Book:  (p) => (<svg width="18" height="18" {...ICON_PROPS} {...p}><path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20"/></svg>),
   Layers:(p) => (<svg width="18" height="18" {...ICON_PROPS} {...p}><polygon points="12 2 2 7 12 12 22 7 12 2"/><polyline points="2 17 12 22 22 17"/><polyline points="2 12 12 17 22 12"/></svg>),
